d81e787b637002d68292f5fb60acad75e6656fa2
Amar <Amarjeet@Amarjeet-PC>
2021-05-16 10:30:59 +0530

    First time uploading

---
+++ HPsatyamLPG/src/test/java/com/satyam/HPsatyamLpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satyam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HPsatyamLpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
