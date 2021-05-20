664dcf96cf9fd64346cacf8dce12d1b360f4b399
amar8988 <73456175+amar8988@users.noreply.github.com>
2021-05-20 17:41:27 +0530

    Delete InstituteInfo directory

--- InstituteInfo/StudentPayRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kush.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.kush.model.StudentPayList;
-
-public interface StudentPayRepository extends CrudRepository<StudentPayList, Integer> {
-
-}
